fc9af8b2f0f9ab4bee7cbaf984467ae29a3f3f8f
Huy Lê Đình <huy.ledinh@hcmut.edu.vn>
2025-02-21 15:40:26 +0700

    fix/build-bug

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), // '@' đại diện cho thư mục src
     },
   },
+  build: {
+    outDir: "build", // Đổi từ "dist" thành "build"
+  },
 })
