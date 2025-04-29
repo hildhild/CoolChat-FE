394b347fe3302cac70f67bc70c1d4be9712462ba
Huy Lê Đình <huy.ledinh@hcmut.edu.vn>
2025-04-29 15:57:47 +0700

    hotfix

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -2,14 +2,21 @@ import { DefaultLayout } from "../../layouts";
 import { Company } from "./Company";
 import { Member } from "./Member";
 import { Mission } from "./Mission";
+import { useEffect } from "react";
 
 export const AboutUs = () => {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+    });
+  }, []);
+
   return (
     <DefaultLayout>
       <div className="mt-16 min-h-[100vh] overflow-x-hidden">
-        <Company/>
-        <Mission/>
-        <Member/>
+        <Company />
+        <Mission />
+        <Member />
       </div>
     </DefaultLayout>
   );
